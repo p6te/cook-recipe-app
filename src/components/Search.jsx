29e15ce1cf1818b6a91d1c9ce72ecd60d41e8cb5
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-06-06 17:57:47 +0200

    add searching function

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
-
+import { useNavigate } from "react-router-dom";
 import "../sass/search.scss";
 
 export default function Search() {
   const [input, setInput] = useState("");
 
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    navigate("/searched/" + input);
   };
+
   const handleInput = (e) => {
     e.preventDefault();
+    setInput(e.target.value);
   };
   return (
     <form className="search" onSubmit={handleSubmit}>
--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Searched() {
-    return (
-     <div>sdfsdfsdf</div>   
-    )
+  const [search, setSearch] = useState([]);
+
+  let params = useParams();
+
+  const getSearched = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`
+    );
+
+    const recipes = await data.json();
+
+    setSearch(recipes.results);
+
+    console.log(search);
+  };
+
+  useEffect(() => {
+    getSearched(params.search);
+  }, [params.search]);
+
+  function Results() {
+    return search.map((item) => {
+      return (
+        <div className="card" key={item.id}>
+          <img src={item.image} alt={item.title} />
+          <h4>{item.title}</h4>
+        </div>
+      );
+    });
+  }
+
+  return (
+    <div className="search">
+      {search.length === 0 ? <h3>There is no results...</h3> : <Results />}
+    </div>
+  );
 }
 
-export default Searched
+export default Searched;
